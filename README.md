a733f8993456a292a5d494cd4c14c17460e309bc
Roman Holdos <rholdos@outlook.sk>
2022-02-27 09:13:41 +0100

    Init project, add images, icons, fonts, base CSS

---
+++ README.md
@@ -0,0 +1,3 @@
+# GoodBoy - Simple React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/app.js
@@ -0,0 +1,11 @@
+import './app.scss';
+
+function App() {
+  return (
+    <section className='app'>
+      <h1>GoodBoy App</h1>
+    </section>
+  );
+}
+
+export default App;
